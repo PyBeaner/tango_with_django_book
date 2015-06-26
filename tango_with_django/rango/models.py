4090f6e7317da03a08eab365f739016ed4ec8163
XiaomingSu <xiaoming.su@patpat.com>
2015-06-26 15:39:44 +0800

    slugify category's name

--- tango_with_django/rango/models.py
+++ tango_with_django/rango/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.text import slugify
+
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
