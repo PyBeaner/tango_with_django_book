13ddbc7977681306bf10064f19d64afc9c8276db
XiaomingSu <xiaoming.su@patpat.com>
2015-06-26 14:37:45 +0800

    templates render

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    # return HttpResponse("Rango says hey there world!")
+
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, "rango/index.html",context=context_dict)
