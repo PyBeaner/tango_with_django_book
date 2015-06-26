1f95ccfcae6e5c91ea6721eb00505cdb42a113a5
XiaomingSu <xiaoming.su@patpat.com>
2015-06-26 16:24:22 +0800

    add page

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Category, Page
-from .forms import CategoryForm
+from .forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -31,8 +31,23 @@ def add_category(request):
     if request.method == "POST":
         form = CategoryForm(request.POST)
         if form.is_valid():
-            form.save(commit=True)
+            form.save()  # By default: commit
             return index(request)
     else:
         form = CategoryForm()
     return render(request, "rango/add_category.html", {"form": form})
+
+
+def add_page(request, category_name_slug):
+    cat = get_object_or_404(Category, slug=category_name_slug)
+    if request.method == "POST":
+        form = PageForm(request.POST)
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.category = cat
+            page.save()
+            return category(request, category_name_slug)
+    else:
+        form = PageForm()
+    context = {"form": form, "category": cat}
+    return render(request, "rango/add_page.html", context)
