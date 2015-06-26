a9a83512340c583620f8f253f727d43343a59227
XiaomingSu <xiaoming.su@patpat.com>
2015-06-26 15:21:43 +0800

    show categories

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -2,8 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Category
+
+
 def index(request):
     # return HttpResponse("Rango says hey there world!")
 
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    return render(request, "rango/index.html",context=context_dict)
+    # context_dict = {'boldmessage': "I am bold font from the context"}
+    # return render(request, "rango/index.html",context=context_dict)
+
+    categories = Category.objects.all()
+    context = {"categories": categories}
+    return render(request, "rango/index.html", context=context)
