82468efa2a7f6cd25ab0fa9519594a5e2882ef45
PyBeaner <758046738@qq.com>
2015-06-27 21:11:14 +0800

    templatetags:get category list

---
+++ tango_with_django/rango/templatetags/rango_extras.py
@@ -0,0 +1,10 @@
+__author__ = 'PyBeaner'
+from django import template
+from ..models import Category
+
+register = template.Library()
+
+
+@register.inclusion_tag("rango/cats.html")
+def get_category_list():
+    return {"cats": Category.objects.all()}
